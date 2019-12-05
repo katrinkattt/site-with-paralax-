ea83f1d50d4ff5d8f586d01ea79dcdfe4db99886
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2019-12-06 00:00:19 +0500

    Create state.js

---
+++ state/js/state.js
@@ -0,0 +1,14 @@
+const initialState = {
+    modalType:'add',
+    currentId:0,
+    comments:[
+        {
+        text:'is like',
+        date: '15:47 03'
+        },
+        {
+        text:'the beatifull',
+        date: '15:57 03'
+        },
+    ]
+}
